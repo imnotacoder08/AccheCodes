b42c09ff6c1b097253fadcbe6ac013809004e289
imnotacoder08 <88921957+imnotacoder08@users.noreply.github.com>
2022-09-23 19:30:04 +0530

    Time: 7 ms (17.91%), Space: 61.5 MB (5.97%) - LeetHub

--- 927-three-equal-parts/927-three-equal-parts.java
+++ 927-three-equal-parts/927-three-equal-parts.java
@@ -1,24 +1,12 @@
 class Solution {
-    public int[] threeEqualParts(int[] arr) {
-        List<Integer> ones = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++){
-            if (arr[i]==1){
-                ones.add(i);
-            }
+    public int concatenatedBinary(int n) {
+        final long modulo = (long) (1e9 + 7);
+        long result = 0;
+        int binaryDigits = 0;
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) binaryDigits++;
+            result = ((result << binaryDigits) + i) % modulo;
         }
-        if (ones.size()==0){ // edge case
-            return new int[]{0,2};
-        }
-        int[] ans = new int[2];
-        int each = ones.size()/3;
-        for (int i = 0; i < 2 && ones.size()%3==0; i++){ // for the first 2 partitions
-            for (int j = 0; j < each-1; j++){ // compare gaps
-                if (ones.get(j+1+i*each)-ones.get(j+i*each)!=ones.get(j+2*each+1)-ones.get(j+2*each))
-                    return new int[]{-1, -1};
-            }
-            ans[i]=ones.get((i+1)*each-1)+i+(arr.length - 1 - ones.get(ones.size()-1)); // cut point
-        }
-        return ones.size()%3>0||ans[0]>=ones.get(each)||ans[1]>ones.get(2*each)?
-            new int[]{-1, -1} : ans;
+        return (int) result;
     }
 }
